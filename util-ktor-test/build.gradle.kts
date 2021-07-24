fbd49ee6810cbda400c1c2653897258862971c2c
Matthias Geisler <matthias.geisler@data4life.care>
2021-07-24 08:23:39 +0200

    Add util-ktor-test

--- util-ktor-test/build.gradle.kts
+++ util-ktor-test/build.gradle.kts
@@ -45,6 +45,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(Dependencies.multiplatform.kotlin.stdlibCommon)
+                implementation(Dependencies.multiplatform.ktor.common)
             }
         }
         val commonTest by getting {
@@ -53,6 +54,8 @@ kotlin {
             dependencies {
                 implementation(Dependencies.multiplatform.kotlin.testCommon)
                 implementation(Dependencies.multiplatform.kotlin.testCommonAnnotations)
+                implementation(Dependencies.multiplatform.ktor.mock)
+                implementation(api(project(":util-coroutine-test"))!!)
             }
         }
 
@@ -134,32 +137,5 @@ android {
             java.setSrcDirs(setOf("src/androidTest/kotlin"))
             res.setSrcDirs(setOf("src/androidTest/res"))
         }
-    }
-}
-
-val templatesPath = "${projectDir}/src/commonTest/resources/templates"
-val configPath = "${projectDir}/src-gen/commonTest/kotlin/care/data4life/sdk/util/test/config"
-
-val provideTestConfig: Task by tasks.creating {
-    doFirst {
-        val templates = File(templatesPath)
-        val configs = File(configPath)
-
-        val config = File(templates, "TestConfig.tmpl")
-            .readText()
-            .replace("PROJECT_DIR", projectDir.toPath().toAbsolutePath().toString())
-
-        if (!configs.exists()) {
-            if(!configs.mkdir()) {
-                System.err.println("The script not able to create the config directory")
-            }
-        }
-        File(configPath, "TestConfig.kt").writeText(config)
-    }
-}
-
-tasks.withType(org.jetbrains.kotlin.gradle.dsl.KotlinCompile::class.java) {
-    if (this.name.contains("Test")) {
-        this.dependsOn(provideTestConfig)
     }
 }
