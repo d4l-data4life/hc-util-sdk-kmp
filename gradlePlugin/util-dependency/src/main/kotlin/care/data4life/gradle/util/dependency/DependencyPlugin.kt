13cd3da2fa9d957b4683c24134999a4e648b39c1
Matthias Geisler <matthias.geisler@data4life.care>
2021-09-15 15:16:35 +0200

    Change Plugin and Script setup

--- gradlePlugin/util-dependency/src/main/kotlin/care/data4life/gradle/util/dependency/DependencyPlugin.kt
+++ gradlePlugin/util-dependency/src/main/kotlin/care/data4life/gradle/util/dependency/DependencyPlugin.kt
@@ -14,7 +14,7 @@
  * contact D4L by email to help@data4life.care.
  */
 
-package care.data4life.sdk.util.dependency
+package care.data4life.gradle.util.dependency
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
