f0f9752c2777c3397202ddf5bab0458c6d836f30
Matthias Geisler <matthias.geisler@data4life.care>
2021-09-23 16:47:31 +0200

    Bump Desugar

--- gradlePlugin/util-dependency/src/main/kotlin/care/data4life/gradle/util/dependency/Version.kt
+++ gradlePlugin/util-dependency/src/main/kotlin/care/data4life/gradle/util/dependency/Version.kt
@@ -38,7 +38,7 @@ object Version {
     /**
      *  https://developer.android.com/studio/write/java8-support
      */
-    const val androidDesugar = "1.0.4"
+    const val androidDesugar = "1.0.9"
 
     // AndroidX
     /**
