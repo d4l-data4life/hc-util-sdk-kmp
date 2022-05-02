794f319995ee1dee95596b4b1f0d202fa97435a2
Matthias Geisler <matthias.geisler@data4life.care>
2022-05-02 11:08:43 +0200

    Bump Test Util

--- gradlePlugin/util-dependency/src/main/kotlin/care/data4life/gradle/util/dependency/Version.kt
+++ gradlePlugin/util-dependency/src/main/kotlin/care/data4life/gradle/util/dependency/Version.kt
@@ -32,7 +32,7 @@ object Version {
     /**
      * https://github.com/d4l-data4life/hc-test-util-sdk-kmp
      */
-    const val sdkTestUtil = "0.4.0"
+    const val sdkTestUtil = "0.4.1"
 
     // Android
     /**
